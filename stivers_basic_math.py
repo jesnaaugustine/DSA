91e898e19c1cf5f3faf823d2ae5a7223cdf92eee
jesnaaugustine <jesnatrisa@gmail.com>
2025-04-29 15:12:09 +0530

    gcd

--- stivers_basic_math.py
+++ stivers_basic_math.py
@@ -41,9 +41,47 @@ def is_palindrome_2(N):
         if n_s[i] != n_s[len(n_s)-1-i]:
             return False
     return result
+#######
+#GCD of two numbers
+#get prime factors of number then get 
+from prime_calculator import Prime
+from functools import reduce
+def gcd(N1,N2):
+    prime1= Prime(N1)
+    factors1 = prime1.prime_factors()
+    prime2 = Prime(N2)
+    factors2 = prime2.prime_factors()
+    fact_set1 = set(factors1.keys())
+    fact_set2 = set(factors2)
+    c_set = fact_set1.intersection(fact_set2)
+    if len(c_set)==0:
+        return 1
+    result = reduce(lambda x,y: (x**(min(factors1[x],factors2[x])))*(y**(min(factors1[y],factors2[y]))),c_set)
+    return result
+
+#method2 - gcd of two numbers equals to gcd of min number subr=trascted from other
+''' 
+Eg, n1 = 20, n2 = 15:
+
+gcd(20, 15) = gcd(20-15, 15) = gcd(5, 15)
+
+gcd(5, 15) = gcd(15-5, 5) = gcd(10, 5)
+
+gcd(10, 5) = gcd(10-5, 5) = gcd(5, 5)
+
+gcd(5, 5) = gcd(5-5, 5) = gcd(0, 5)
+
+Hence, return 5 as the gcd.
+'''
+def gcd_2(N1,N2):
+    if N1 ==0 or N2 ==0:
+        return max(N1,N2)
+    return gcd_2(min(N1,N2),max(N1,N2)-min(N1,N2))
+    
 
 
 if __name__=='__main__':
     #print(count_digits_2(1000))
     #print(reverse(10002))
-    print(is_palindrome_2(1001))
+    #print(is_palindrome_2(1001))
+    print(gcd_2(20,10))

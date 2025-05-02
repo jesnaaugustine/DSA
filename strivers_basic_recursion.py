7767dee7f624cd20a4b6be842506ffd5495f3787
jesnaaugustine <jesnatrisa@gmail.com>
2025-05-02 15:56:09 +0530

    recursion 2

--- strivers_basic_recursion.py
+++ strivers_basic_recursion.py
@@ -51,10 +51,41 @@ def reverse(arr):
         return [arr[i]] +inner(i-1)
         
     return inner(len(arr)-1)
+######################
+
+#check if the string is palindome
+def is_palindrome(s):
+    def inner(s,l,r):
+        if l>=r:
+            return True
+        if s[l]!=s[r]:
+            return False
+        return inner(s,l+1,r-1)
+        
+    return inner(s, 0, len(s)-1)  
+
+#####################
+#Fibonacci Series up to Nth term
+def fibonacci(N):
+    def inner(n):
+        if n<=1:
+            return n
+        return inner(n-1)+inner(n-2)
+    res =[]
+    for i in range(0,N+1):
+        res.append(inner(i))
+
+    return res
+    
+
+
+    
 
 
 if __name__=='__main__':
     #print_name(10,'Jesna Augustine')
     #one_n(6)
     #N_one(3)
-    print(reverse([1,2,3,4]))
+    #print(reverse([1,2,3,4]))
+    #print(is_palindrome('masam'))
+    print(fibonacci(6))

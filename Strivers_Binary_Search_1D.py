fdb23c060cd7bb874d11aaad63d66b5d721cb44c
jesnaaugustine <jesnatrisa@gmail.com>
2025-06-03 17:30:11 +0530

    BS on answer 4

--- Strivers_Binary_Search_1D.py
+++ Strivers_Binary_Search_1D.py
@@ -463,7 +463,34 @@ def bouquet_make(arr,m,k):
     return -1
 
 
+#############
+#Find the Smallest Divisor Given a Threshold
+'''
+Problem Statement: You are given an array of integers 'arr' and an integer i.e. a threshold value 'limit'. Your task is to find the smallest positive integer divisor, such that upon dividing all the elements of the given array by it, the sum of the division's result is less than or equal to the given threshold value.
 
+Example 1:
+Input Format: N = 5, arr[] = {1,2,3,4,5}, limit = 8
+Result: 3
+Explanation: We can get a sum of 15(1 + 2 + 3 + 4 + 5) if we choose 1 as a divisor. 
+The sum is 9(1 + 1 + 2 + 2 + 3)  if we choose 2 as a divisor. Upon dividing all the elements of the array by 3, we get 1,1,1,2,2 respectively. Now, their sum is equal to 7 <= 8 i.e. the threshold value. So, 3 is the minimum possible answer.
+
+'''
+def smallest_divisor(arr,limit):
+    l = 1
+    r = max(arr)
+    result = max(arr)
+    while l<=r:
+        mid =(l+r)//2
+        ans =0
+        for i in arr:
+            ans+= (i//mid if i%mid ==0 else (i//mid)+1)
+        if ans >limit:
+            l =mid+1
+        else:
+            result =min(result,mid)
+            r=mid-1
+    return result
+        
 
 if __name__=='__main__':
     #print(binary_search([3, 4, 6, 7, 9, 12, 16, 17],1))
@@ -481,4 +508,5 @@ if __name__=='__main__':
     #print(single_ele([1,2,2,3,3,4,4,5,5,6,6]))
     #print(sqrt(36))
     #print(minEatingSpeed([30,11,23,4,20],6))
-    print(bouquet_make([1, 10, 3, 10, 2],2,3))
+    #print(bouquet_make([1, 10, 3, 10, 2],2,3))
+    print(smallest_divisor([8,4,2,3],10))

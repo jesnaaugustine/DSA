8fb4be8fb208b66efe93c5ebcb12b1c4f8b3191b
jesnaaugustine <jesnatrisa@gmail.com>
2025-12-31 15:46:01 +0530

    binary tree 2

--- strivers_binary_tree.py
+++ strivers_binary_tree.py
@@ -21,3 +21,44 @@ class Solution(object):
             inner(root_in.right)
         inner(root)
         return ans
+    
+
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        ans =[]
+        def inner(root_in):
+            if root_in is None:
+                return
+            inner(root_in.left)
+            ans.append(root_in.val)
+            inner(root_in.right)
+        inner(root)
+        return ans
+    
+
+    def postorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        ans =[]
+        def inner(root_in):
+            if root_in is None:
+                return
+            inner(root_in.left)
+            inner(root_in.right)
+            ans.append(root_in.val)
+        inner(root)
+        return ans
+    
+if __name__=='__main__':
+    root = TreeNode(val =1,left =None,right =None)
+    right = TreeNode(val =2,left =TreeNode(3),right =None)
+    root.right =right
+    sol = Solution()
+    print(sol.preorderTraversal(root))
+    print(sol.inorderTraversal(root))
+    print(sol.postorderTraversal(root))

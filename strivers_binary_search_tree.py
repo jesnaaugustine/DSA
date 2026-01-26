0e608352c938a0da6ffc25ad7864fb961bc4fd9f
jesnaaugustine <jesnatrisa@gmail.com>
2026-01-26 22:36:36 +0530

    insert into tree

--- strivers_binary_search_tree.py
+++ strivers_binary_search_tree.py
@@ -19,9 +19,31 @@ def searchBST(root, val):
         else:
             temp = temp.right
     return temp
-
+def insertIntoBST(root, val):
+        """
+        :type root: Optional[TreeNode]
+        :type val: int
+        :rtype: Optional[TreeNode]
+        """
+        temp =root
+        parent = None
+        if root is None:
+            return TreeNode(val)
+        while temp:
+            parent = temp
+            if temp.val<val:
+                temp=temp.right
+            elif temp.val>val:
+                temp =temp.left
+        if parent.val<val:
+            parent.right = TreeNode(val)
+        else:
+            parent.left = TreeNode(val)
+        return root
+            
 if __name__=='__main__':
     root = TreeNode(val =4,left =None,right =None)
     right = TreeNode(val =6,left =TreeNode(5),right =None)
     root.right =right
     print(searchBST(root, 6))
+    print(insertIntoBST(root,3))

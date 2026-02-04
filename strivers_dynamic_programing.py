e51a50fea161b2dbc0cf6661a1a1ab237740a0f6
jesnaaugustine <jesnatrisa@gmail.com>
2026-02-04 22:23:22 +0530

    dp2

--- strivers_dynamic_programing.py
+++ strivers_dynamic_programing.py
@@ -13,8 +13,22 @@ def climbStairs(n):
         prev2=prev1
         prev1 =temp
     return prev1
-
-
+#198. House Robber
+def rob(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    if len(nums)<2:
+        return max(nums)
+    dp=[0]*len(nums)
+    dp[0]=nums[0]
+    dp[1]= max(nums[0],nums[1])
+    for i in range(2,len(nums)):
+        dp[i]=max(dp[i-1],dp[i-2]+nums[i])
+    return dp[-1]
 if __name__ =='__main__':
     print(climbStairs(3))
+    nums =[1,2,3,1]
+    print(rob(nums))
         

f35e626153426d44e534a032395756ca1a3cba0f
jesnaaugustine <jesnatrisa@gmail.com>
2026-02-06 23:06:56 +0530

    frog jump

--- strivers_dynamic_programing.py
+++ strivers_dynamic_programing.py
@@ -45,10 +45,27 @@ def rob2(nums):
     temp1 =nums[:-1]
     temp2 = nums[1:]
     return max(robthis(temp1),robthis(temp2))
+''' 
+Problem Statement: Given a number of stairs and a frog, the frog wants to climb from the 0th stair to the (N-1)th stair. At a time the frog can climb either one or two steps. A height[N] array is also given. Whenever the frog jumps from a stair i to stair j, the energy consumed in the jump is abs(height[i]- height[j]), where abs() means the absolute difference. We need to return the minimum energy that can be used by the frog to jump from stair 0 to stair N-1..
+'''
+def FrogJump(nums):
+    if len(nums)==1:
+        return 0
+    if len(nums)==2:
+        return abs(nums[0]-nums[1])
+    prev2 = 0
+    prev1 = abs(nums[0]-nums[1])
+    for i in range(2,len(nums)):
+        cur = min(prev1+abs(nums[i]-nums[i-1]),prev2+abs(nums[i]-nums[i-2]))
+        prev2 = prev1
+        prev1 =cur
+    return prev1
     
 if __name__ =='__main__':
     print(climbStairs(3))
     nums =[1,2,3,1]
     print(rob(nums))
     print(rob2(nums))
+    nums = [2,1,3,5,4]
+    print(FrogJump(nums))
         

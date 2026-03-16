95e73084ecbcf3ce8186ccb9852e4df893b981bc
jesnaaugustine <jesnatrisa@gmail.com>
2026-03-16 23:13:42 +0530

    num distinct

--- strivers_dynamic_programing.py
+++ strivers_dynamic_programing.py
@@ -511,7 +511,27 @@ def shortestCommonSupersequence(str1, str2):
         j-=1
     re_ans = ans[::-1]
     return ''.join(re_ans)
-        
+def numDistinct(s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: int
+    """
+    m=len(s)
+    n =len(t)
+    dp =[0]*(n+1)
+    dp[0]=1
+    for i in range(1,m+1):
+        temp_dp =[0]*(n+1)
+        temp_dp[0]=1
+        for j in range(1,n+1):
+            if s[i-1]==t[j-1]:
+                temp_dp[j]=dp[j-1]+dp[j]
+            else:
+                temp_dp[j]=dp[j]
+        dp=temp_dp
+    return dp[-1]
+    
 
         
 if __name__ =='__main__':
@@ -565,6 +585,9 @@ if __name__ =='__main__':
     str1 = "dynamic"
     str2 = "dyamic"
     print(shortestCommonSupersequence(str1,str2))
+    s ='babgbag'
+    t ='bag'
+    print(numDistinct(s,t))
 
 
 
